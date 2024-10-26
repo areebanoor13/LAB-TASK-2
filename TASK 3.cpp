2b736a7fbbb043394dc7f2a60dae66f706f7cded
Areeba Noor <63746@students.riphah.edu.pk>
2024-10-26 23:30:32 +0500

    Initial commit

---
+++ TASK 3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+	string name="Areeba";
+	int sapId=63746;
+	string semester="First";
+	string course="BSCS";
+	cout<<name;
+    cout<<sapId;
+    cout<<course;
+    cout<<semester;
+return 0;
+}
